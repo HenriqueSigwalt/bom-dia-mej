34b168eb81fc8d4afa671c3d2fa1ef03fed37e83
HenriqueSigwalt <140031473+HenriqueSigwalt@users.noreply.github.com>
2026-04-02 11:22:35 -0300

    1

--- views.py
+++ views.py
@@ -1,10 +1,10 @@
 from flask import Flask
-#from main import *
+from main import *
 import os
 
 app=Flask(__name__)
 
 @app.route("/")
 def land():
-    #asyncio.run(main())
-    return os.environ.get("API_HASH")
+    asyncio.run(main())
+    return "OI"
